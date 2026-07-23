d1797a9d2a0c55a84889e118abbd8ae3891c1a63
Tarek Farg <144064925+TarekFarg@users.noreply.github.com>
2026-07-23 14:44:25 +0300

    Time: 0 ms (100%), Space: 9.6 MB (17.14%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size() , n = matrix[0].size() ;
+        vector<vector<bool>> vist(m,vector<bool>(n));
+        vector<int> ans ;
+        int curDir = 0 , i = 0 , j = 0 , count = 0 ;
+        while(count < (n*m))
+        {
+            if(!vist[i][j])
+            {
+                vist[i][j] = true ;
+                ans.push_back(matrix[i][j]) ;
+                count++;
+            }
+            
+            if(curDir%4 == 0) // R
+            {
+                if(j+1 < n && !vist[i][j+1])
+                    j++;
+                else
+                    curDir++ ;
+            }
+            else if(curDir%4 == 1) // D
+            {
+                if(i+1 < m && !vist[i+1][j])
+                    i++;
+                else
+                    curDir++;
+            }
+            else if(curDir%4 == 2) // L
+            {
+                if(j-1>=0 && !vist[i][j-1])
+                    j-- ;
+                else
+                    curDir++ ;
+            }
+            else // U
+            {
+                if(i-1>=0 && !vist[i-1][j])
+                    i--;
+                else
+                    curDir++;
+            }
+        }
+        return ans;
+    }
+};
+
+// R -> D -> L -> U -> R -------
+// %4 -> 0 -> R
+// %4 -> 1 -> D
+// %4 -> 2 -> L
+// %4 -> 3 -> U
+
