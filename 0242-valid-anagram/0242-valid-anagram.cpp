e5414a7865f70b669bf10b08dbccd2c19403a13f
Tarek Farg <144064925+TarekFarg@users.noreply.github.com>
2026-08-26 20:28:10 +0300

    Time: 0 ms (100%), Space: 9.8 MB (49.53%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+            return false ;
+        
+        vector<int> freq1(26) , freq2(26) ;
+        for(char ch : s)
+            freq1[ch-'a']++;
+        for(char ch : t)
+            freq2[ch-'a']++;
+        for(int i = 0 ; i < 26 ; i++)
+        {
+            if(freq1[i]!=freq2[i])
+                return false; 
+        }
+        return true;
+    }
+};
